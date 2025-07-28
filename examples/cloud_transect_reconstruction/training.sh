b368105c212497cb4f8d34cdfff795d1f73f618d
szhanggg <stevez9101@gmail.com>
2025-07-28 13:44:41 -0400

    adding pipeline scripts

---
+++ examples/cloud_transect_reconstruction/training.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=unet-training
+#SBATCH --time=72:00:00
+
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=64gb
+module load anaconda
+conda activate torch
+
+srun python3 unetpipeline.py
